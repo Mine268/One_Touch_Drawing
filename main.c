69cbac20a930387b77745a8e99a42ae690a436a2
Mine268 <1106942614@qq.com>
2017-04-22 13:26:37 +0800

    add comments

--- main.c
+++ main.c
@@ -2,27 +2,33 @@
 
 typedef enum { false, true } bool;
 
-bool flag = false;
+bool flag = false; // indicate whether the drawing could be drawn in one path
 int e[101][101], n, m, c;
+/*
+ * 'e' for the picture
+ * 'n' for points
+ * 'm' for lines
+ * 'c' for the lines that hace been searched
+*/
 void search(int cur);
 void printOut();
 
 int main() {
 	scanf("%d %d", &n, &m);
 	int i, j;
-	for (i = 1; i <= n; i++)
-	for (j = 1; j <= n; j++)
-		e[i][j] = 0;
+	for (i = 1; i <= n; i++) // init the square
+		for (j = 1; j <= n; j++)
+			e[i][j] = 0;
 
 	int a, b;
-	for (i = 1; i <= m; i++) {
+	for (i = 1; i <= m; i++) { // read the data
 		scanf("%d %d", &a, &b);
 		e[a][b]++;
 		e[b][a]++;
 	}
 
 	int k;
-	for (k = 1; k <= m; k++)
+	for (k = 1; k <= m; k++) // begin the search from each point
 		search(k);
 
 	if (flag == false)
@@ -35,18 +41,18 @@ int main() {
 	return 0;
 }
 
-void search(int cur) {
+void search(int cur) { // core part
 	int i;
-	if (c == m) {
+	if (c == m) { // examine if all the lines have been searched
 		flag = true;
 		return;
 	}
 
 	for (i = 1; i <= n; i++) {
-		if (e[cur][i] > 0) {
-			e[cur][i]--;
+		if (e[cur][i] > 0) { // the lines No.'e[cur][i]'
+			e[cur][i]--; // means that this line has been searched for one time
 			e[i][cur]--;
-			c++;
+			c++; // means the program has searched one line
 			search(i);
 			c--;
 			e[cur][i]++;
@@ -56,7 +62,7 @@ void search(int cur) {
 	return;
 }
 
-void printOut() {
+void printOut() { // print the square for debugging
 	int i, j;
 	for (i = 1; i <= n; i++) {
 		for (j = 1; j <= n; j++) {
